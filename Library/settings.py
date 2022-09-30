a3edb0caecb4c0df16583d48b4e3c2585b4b31f4
Tomas Smolskas <tomas.smolskas@gmail.com>
2022-09-30 14:54:26 +0300

    Some changes

--- Library/settings.py
+++ Library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7(8jy$85tu_o&8eq*t$1q9ti*aqg65t#3nu-exq0!w1sp0e5p#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
